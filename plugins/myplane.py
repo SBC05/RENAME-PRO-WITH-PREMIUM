9401674758d4dea0ee27c3e8047cbcad83eabaf5
OfficialVivekTomar <116698896+OfficialVivekTomar@users.noreply.github.com>
2023-02-05 16:33:12 +0530

    Update myplane.py

--- plugins/myplane.py
+++ plugins/myplane.py
@@ -34,10 +34,10 @@ async def start(client,message):
 	        uploadlimit(message.from_user.id,2147483648)
 	        usertype(message.from_user.id,"Free")
 	if ends == None:
-	    text = f"𝐔𝐬𝐞𝐫 𝐈𝐃 : ```{message.from_user.id}```\n𝐏𝐥𝐚𝐧 : {user}\n𝐃𝐚𝐢𝐥𝐲 𝐔𝐩𝐥𝐨𝐚𝐝 𝐋𝐢𝐦𝐢𝐭 : {humanbytes(limit)}\n𝐓𝐨𝐝𝐚𝐲 𝐔𝐬𝐞𝐝 : {humanbytes(used)}\n𝐑𝐞𝐦𝐚𝐢𝐧𝐢𝐧𝐠 : {humanbytes(remain)}"
+	    text = f"𝐔𝐬𝐞𝐫 𝐈𝐃 : ```{message.from_user.id}```\n\n𝐏𝐥𝐚𝐧 : {user}\n\n𝐃𝐚𝐢𝐥𝐲 𝐔𝐩𝐥𝐨𝐚𝐝 𝐋𝐢𝐦𝐢𝐭 : {humanbytes(limit)}\n\n𝐓𝐨𝐝𝐚𝐲 𝐔𝐬𝐞𝐝 : {humanbytes(used)}\n\n𝐑𝐞𝐦𝐚𝐢𝐧𝐢𝐧𝐠 : {humanbytes(remain)}"
 	else:
 	    normal_date = datetime.fromtimestamp(ends).strftime('%Y-%m-%d')
-	    text = f"𝐔𝐬𝐞𝐫 𝐈𝐃 : ```{message.from_user.id}```\n𝐏𝐥𝐚𝐧 : {user}\n𝐃𝐚𝐢𝐥𝐲 𝐔𝐩𝐥𝐨𝐚𝐝 𝐋𝐢𝐦𝐢𝐭 : {humanbytes(limit)}\n𝐓𝐨𝐝𝐚𝐲 𝐔𝐬𝐞𝐝 : {humanbytes(used)}\n𝐑𝐞𝐦𝐚𝐢𝐧𝐢𝐧𝐠 : {humanbytes(remain)}\n\n```𝐘𝐨𝐮𝐫 𝐏𝐥𝐚𝐧 𝐄𝐧𝐝𝐬 𝐎𝐧 : {normal_date}"
+	    text = f"𝐔𝐬𝐞𝐫 𝐈𝐃 : ```{message.from_user.id}```\n\n𝐏𝐥𝐚𝐧 : {user}\n\n𝐃𝐚𝐢𝐥𝐲 𝐔𝐩𝐥𝐨𝐚𝐝 𝐋𝐢𝐦𝐢𝐭 : {humanbytes(limit)}\n\n𝐓𝐨𝐝𝐚𝐲 𝐔𝐬𝐞𝐝 : {humanbytes(used)}\n\n𝐑𝐞𝐦𝐚𝐢𝐧𝐢𝐧𝐠 : {humanbytes(remain)}\n\n```𝐘𝐨𝐮𝐫 𝐏𝐥𝐚𝐧 𝐄𝐧𝐝𝐬 𝐎𝐧 : {normal_date}"
 	    
 	if user == "Free":
 	    await message.reply(text,quote = True,reply_markup = InlineKeyboardMarkup([[       			InlineKeyboardButton("Upgrade 💰💳",callback_data = "upgrade"), InlineKeyboardButton("Cancel ✖️ ",callback_data = "cancel") ]]))
